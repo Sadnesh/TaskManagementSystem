1c9356facc909aeacd7f864d71da8a176e553a48
sadnesh <sandeshbhantana@gmail.com>
2025-03-01 15:23:49 +0545

    [INIT]: initial commit

---
+++ Models/Tasks.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class Tasks
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
